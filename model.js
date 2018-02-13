4525034233d7dd97d32b1e5aa8cfb02fda05d3d3
Christian Monzón Cárdenas <cmonzon3@gmail.com>
2018-02-13 01:37:48 +0100

    Update model

--- model.js
+++ model.js
@@ -1,3 +1,60 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+var canvas = document.getElementById('myCanvas');
+var editingMode = { rect: 0, line: 1 };
+
+function Pencil(ctx, drawing, canvas) {
+	console.log("a");
+
+	this.currEditingMode = editingMode.line;
+	this.currLineWidth = 5;
+	this.currColour = '#000000';
+	this.currentShape = 0;
+
+	// Liez ici les widgets à la classe pour modifier les attributs présents ci-dessus.
+
+//	new DnD(canvas, this);
+
+
+
+	// Implémentez ici les 3 fonctions onInteractionStart, onInteractionUpdate et onInteractionEnd
+
+
+};
+
+	function getMousePosition(canvas, evt){
+
+		var rect = canvas.getBoundClientRect();
+
+		console.log(evt.x);
+		console.log(evt.y);
+		return {
+			x: evt.x - rect.left, 
+			y: evt.x - rect.top
+		};
+
+	};
+
+	function onInteractionStart(canvas, event){
+		console.log('a');
+
+	};
+
+	function onInteractionUpdate(canvas, event){
+		console.log('b');
+	};
+	
+
+	function onInteractionUpdate(canvas, event){
+		console.log('c');
+	};
+
+	canvas.addEventListener('onclick', this.getMousePosition, false);
+	canvas.addEventListener('mousedown', this.onInteractionStart, false);
+	canvas.addEventListener('mousemove', this.onInteractionEnd, false);
+	canvas.addEventListener('mouseup', this.onInteractionUpdate, false);
+
+
+
